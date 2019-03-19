1faf8808744c4cd73d30e1b8c565964a7b3e9d6e
Amostaral <39526273+Amostaral@users.noreply.github.com>
2019-03-19 18:26:21 +0100

    Add files via upload

--- serveur.php
+++ serveur.php
@@ -1,4 +1,5 @@
 <?php
+
 $aContext = array(
   'http' => array (
     'proxy'=>'tcp://cache.iutbayonne.univ-pau.fr:3128',
@@ -9,7 +10,6 @@ $aContext = array(
     'request_fulluri' => true
   )
 );
-
 $cxContext = stream_context_create($aContext);
 
 function invalidParams() {
@@ -27,12 +27,24 @@ if(isset($_GET['method']) && $_GET['method'] != '') {
 switch ($method) {
 
   case 'getGameInformation':
-  $url = "http://opengamedb.org/api/getGame/1";
+  $url = "http://opengamedb.org/api/getGame/".rand(1,20000);
   $xml = file_get_contents("$url", False, $cxContext);
-  $chaine = new SimpleXMLElement($xml);
+  $chaine = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA );
 
   if(isset($chaine)) {
-	  print "Titre : $chaine->title";
+	  print "Titre : ".$chaine->entry->title."<br>";
+    print "<br> Description : ".$chaine->entry->overview."<br>";
+    print "<br> Nombre de joueur : ".$chaine->entry->players."<br>";
+  }
+  else {
+	  print "Erreur".utf8_decode($xml->error);
+  }
+
+  $url = "http://opengamedb.org/api/getPlatform/".$chaine->entry->platform->id;
+  $xml = file_get_contents("$url", False, $cxContext);
+  $chaine = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA );
+  if(isset($chaine)) {
+	  print "<br> Platform : ".$chaine->entry->name."<br>";
   }
   else {
 	  print "Erreur".utf8_decode($xml->error);
@@ -46,4 +58,3 @@ switch ($method) {
 }
 
 ?>
-)
