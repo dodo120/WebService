09f6d515615a9846c9b6bbebb504fc3f7b1b4757
Amostaral <39526273+Amostaral@users.noreply.github.com>
2019-03-19 15:11:08 +0100

    Add files via upload

--- client.php
+++ client.php
@@ -1,19 +1,19 @@
 <?php
 
 $aContext = array(
-  'http' => array(
-    'proxy' => 'tcp://cache.iutbayonne.univ-pau.fr:31280',
+  'http' => array (
+    'proxy'=>'tcp://cache.iutbayonne.univ-pau.fr:3128',
     'request_fulluri' => true
   ),
-  'http' => array(
-    'proxy' => 'tcp://cache.iutbayonne.univ-pau.fr:31280',
+  'https' => array (
+    'proxy'=>'tcp://cache.iutbayonne.univ-pau.fr:3128',
     'request_fulluri' => true
   )
 );
 
 $cxContext = stream_context_create($aContext);
 
-$url = "./serveur.php?method=getGameInformation";
+$url = "http://iparla.iutbayonne.univ-pau.fr/~mfringant/PHP_AJAX/S4/Webservice/Projet/serveur.php?method=getGameInformation";
 $json = file_get_contents("$url", False, $cxContext);
 
 print $json;
--- serveur.php
+++ serveur.php
@@ -1,4 +1,16 @@
 <?php
+$aContext = array(
+  'http' => array (
+    'proxy'=>'tcp://cache.iutbayonne.univ-pau.fr:3128',
+    'request_fulluri' => true
+  ),
+  'https' => array (
+    'proxy'=>'tcp://cache.iutbayonne.univ-pau.fr:3128',
+    'request_fulluri' => true
+  )
+);
+
+$cxContext = stream_context_create($aContext);
 
 function invalidParams() {
 	$params = array("status"=>0, "msg"=>'Invalid Parameters Supplied');
@@ -15,17 +27,17 @@ if(isset($_GET['method']) && $_GET['method'] != '') {
 switch ($method) {
 
   case 'getGameInformation':
-  $url = "opengamedb.org/api/getGame/1";
+  $url = "http://opengamedb.org/api/getGame/1";
   $xml = file_get_contents("$url", False, $cxContext);
   $chaine = new SimpleXMLElement($xml);
-  
+
   if(isset($chaine)) {
 	  print "Titre : $chaine->title";
   }
   else {
 	  print "Erreur".utf8_decode($xml->error);
   }
-  break:
+  break;
 
   default:
   $params = array("status"=>0, "msg"=>'Invalid Parameters Supplied');
